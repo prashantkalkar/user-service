ea5ee18a54e970e7c54a2d97ae3fb80f4363bf39
Prashant Kalkar <kalkar.prashant@gmail.com>
2018-04-26 12:40:09 +0530

    Implemented provider pact verification test for User service.

---
+++ src/test/java/com/ee/userservice/UserServiceTest.java
@@ -0,0 +1,27 @@
+package com.ee.userservice;
+
+import au.com.dius.pact.provider.junit.Provider;
+import au.com.dius.pact.provider.junit.State;
+import au.com.dius.pact.provider.junit.loader.PactFolder;
+import au.com.dius.pact.provider.junit.target.Target;
+import au.com.dius.pact.provider.junit.target.TestTarget;
+import au.com.dius.pact.provider.spring.SpringRestPactRunner;
+import au.com.dius.pact.provider.spring.target.SpringBootHttpTarget;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@RunWith(SpringRestPactRunner.class)
+@Provider("user_service")
+@PactFolder("/Users/prashant/Contents/ee/cdc-pact-expert-talk/order-service/target/pacts")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UserServiceTest {
+
+    @TestTarget
+    public final Target target = new SpringBootHttpTarget();
+
+    @State("User Paresh exists")
+    public void noState() {
+        // Setup user paresh details in database for the test.
+        System.out.println("Setting up user Paresh in database");
+    }
+}
