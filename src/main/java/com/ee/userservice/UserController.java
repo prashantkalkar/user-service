02e3aea6741e6d6ffeb4b6807b5a4af23005d289
Paresh Mahajan <paresh.mahajan@gmail.com>
2018-05-03 18:14:07 +0530

    Renamed name property to fullName

--- src/main/java/com/ee/userservice/UserController.java
+++ src/main/java/com/ee/userservice/UserController.java
@@ -14,20 +14,20 @@ public class UserController {
 
     public static class User {
 
-        private String name;
+        private String fullName;
         private String email;
         private String contact;
         private String address;
 
         public User(String name, String email, String contact, String address) {
-            this.name = name;
+            this.fullName = name;
             this.email = email;
             this.contact = contact;
             this.address = address;
         }
 
-        public String getName() {
-            return name;
+        public String getFullName() {
+            return fullName;
         }
 
         public String getEmail() {
