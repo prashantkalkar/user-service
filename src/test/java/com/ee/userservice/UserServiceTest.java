3da506be8cdf5c0f2218a0c84d68dca202fd8091
Prashant Kalkar <kalkar.prashant@gmail.com>
2018-05-04 16:39:34 +0530

    Resetting to initial state.

--- src/test/java/com/ee/userservice/UserServiceTest.java
+++ src/test/java/com/ee/userservice/UserServiceTest.java
@@ -3,6 +3,7 @@ package com.ee.userservice;
 import au.com.dius.pact.provider.junit.Provider;
 import au.com.dius.pact.provider.junit.State;
 import au.com.dius.pact.provider.junit.loader.PactBroker;
+import au.com.dius.pact.provider.junit.loader.PactFolder;
 import au.com.dius.pact.provider.junit.target.Target;
 import au.com.dius.pact.provider.junit.target.TestTarget;
 import au.com.dius.pact.provider.spring.SpringRestPactRunner;
@@ -12,8 +13,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @RunWith(SpringRestPactRunner.class)
 @Provider("user_service")
-//@PactFolder("../order-service/target/pacts")
-@PactBroker(host = "broker_app", port = "80", tags = {"latest", "DEV", "PROD"})
+@PactFolder("../order-service/target/pacts")
+//@PactBroker(host = "broker_app", port = "80", tags = {"latest", "DEV", "PROD"})
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserServiceTest {
 
